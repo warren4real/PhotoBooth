05eedc75e110c1f5c0581e7290a0d05a381bb7e8
warren4real <153218516+warren4real@users.noreply.github.com>
2026-09-15 00:51:48 +0800

    Initial Commit

---
+++ PhotoBooth/PhotoBoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoBoothApp.swift
+//  PhotoBooth
+//
+//  Created by Warren Malawae on 9/15/26.
+//
+
+import SwiftUI
+
+@main
+struct PhotoBoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
